ebba3d7def592a76f168d562cdaec33ce6afb401
Fredeto1 <51245330+Fredeto1@users.noreply.github.com>
2019-07-10 21:41:54 -0400

    update

--- js/script.js
+++ js/script.js
@@ -2,12 +2,13 @@
       //Makes the cursor start in the name field
 $('#name').focus();  
 
-//JOB ROLE
+/*JOB ROLE
 //include a text field that will be revealed when the other option is selected from the job role drop down menu
-//Give the field an id of other-title and add the place holder of your job role
-//<input id="other-title" name="job_role_other" placeholder="Your Job Role" type="text">    
-$('#other-title').hide();  
+//Give the field an id of other-title and add the place holder of your job role*/   
 
+//hides the other field unitl it's selected
+$('#other-title').hide();  
+//event listener that is on the title id and listens for when other is clicked.
 $('#title').on('change',function(event){
     if ($(this).val()==='other') {
         $('#other-title').show(); 
@@ -17,6 +18,4 @@ $('#title').on('change',function(event){
     
   });
 
- 
- 
 
