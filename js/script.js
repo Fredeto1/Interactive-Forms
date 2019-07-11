44b617bd685daab665f6534fb7ebecf34ece4008
Fredeto1 <51245330+Fredeto1@users.noreply.github.com>
2019-07-11 08:35:46 -0400

    fixed

--- js/script.js
+++ js/script.js
@@ -1,21 +1,43 @@
  //FOCUS
-      //Makes the cursor start in the name field
+//Makes the cursor start in the name field
 $('#name').focus();  
 
-/*JOB ROLE
-//include a text field that will be revealed when the other option is selected from the job role drop down menu
-//Give the field an id of other-title and add the place holder of your job role*/   
+//JOB ROLE
 
-//hides the other field unitl it's selected
+//hides the other field until it's selected
 $('#other-title').hide();  
-//event listener that is on the title id and listens for when other is clicked.
-$('#title').on('change',function(event){
+//Event listener that is on the title id and listens for when other is clicked. 
+//If the value of the event is equal to other it shows the Your Job Role input field otherwise it hides it.
+$('#title').on('change', function(event){
     if ($(this).val()==='other') {
         $('#other-title').show(); 
     } else {
         $('#other-title').hide();   
     }
-    
   });
 
+//TEE-SHIRT SECTION
+//Hides the  color choices
+$('#colors-js-puns').hide(); 
 
+//The event listener listens for when the design choice changes. and then removes the color choices
+//by setting the value of color to an empty HTML string.
+$('#design').on('change', function () { 
+    $('#color').html(''); 
+//If js puns is selected only show the following colors by appending them to the now empty string
+    if ($(this).val() === 'js puns') { 
+        $('#color').append('<option value = "cornflowerblue">Cornflower Blue (JS Puns shirt only)</option>');
+        $('#color').append('<option value="darkslategrey">Dark Slate Grey (JS Puns shirt only)</option>');
+        $('#color').append('<option value="gold">Gold (JS Puns shirt only)</option>');
+        $('#colors-js-puns').show();
+//If heart js is selected only show the following colors by appending them to the now empty string
+    } else if ($(this).val() === 'heart js') { 
+        $('#color').append('<option value = "tomato">Tomato (I ? JS shirt only)</option>');
+        $('#color').append('<option value = "steelblue">Steel Blue (I ? JS shirt only)</option>');
+        $('#color').append('<option value="dimgrey">Dim Grey (I ? JS shirt only)</option>');
+        $('#colors-js-puns').show();
+//If nothing is selected then hide all the color choices
+    } else {
+        $('#colors-js-puns').hide(); 
+    }
+});
